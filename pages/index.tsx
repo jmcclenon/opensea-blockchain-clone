bbd1d540edfcbdaad4b54e4b964bf61937419502
JohnM <mars.john2018@gmail.com>
2022-06-24 21:12:52 -0400

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <h2>Yo... this is some new shit!ls </h2>
+  )
+}
